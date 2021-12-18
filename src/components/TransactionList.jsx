ca6556151fd764a8a65b7bd5ccbf8af262f7e8a6
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-18 18:44:57 -0500

    map over transactions ui

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useState } from 'react/cjs/react.development';
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
+
+    const { transactions } = useState(GlobalContext);
+
     return (
         <>
         <h3>History</h3>
-        <ul id="list" class="list">
+        {transactions.map(transaction => (
           <li className="minus">
-            Cash <span>-$400</span><button class="delete-btn">x</button>
-          </li>
+          {transaction.text} <span>-$400</span><button class="delete-btn">x</button>
+        </li>
+        ))}
+        <ul id="list" class="list">
         </ul>
         </>
     )
