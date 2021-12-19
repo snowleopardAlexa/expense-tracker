597689a020cdb265fb18ade048bb106b5c2a77fc
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-19 17:49:07 -0500

    fix form

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const AddTransaction = () => {
     return (
-    <div>
-      <h3>Add new transaction</h3>
-      <form id="form">
+        <>
+        <h3>Add new transaction</h3>
+        <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" id="text" placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label for="amount"
+          <label htmlFor="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
           <input type="number" id="amount" placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
-      </form>
-     </div>
+       </form>
+       </>
     )
 }
 
