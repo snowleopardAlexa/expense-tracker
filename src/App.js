457c85c08184aaa84a151f76c89196cf344ad071
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-19 17:39:30 -0500

    add income expenses comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
