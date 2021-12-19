632b7163ca47cc5e4b31f31269f30f3657129804
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-19 17:44:50 -0500

    add ultimate UI

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AddTransaction = () => {
+    return (
+    <div>
+      <h3>Add new transaction</h3>
+      <form id="form">
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" id="text" placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label for="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" id="amount" placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+     </div>
+    )
+}
+
+export default AddTransaction;
+
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -5,7 +5,7 @@ const TransactionList = () => {
         <>
         <h3>History</h3>
         <ul id="list" class="list">
-          <li class="minus">
+          <li className="minus">
             Cash <span>-$400</span><button class="delete-btn">x</button>
           </li>
         </ul>
